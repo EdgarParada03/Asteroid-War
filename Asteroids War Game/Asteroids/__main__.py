c0dfb4bc70a3507888132f9c332f15c01ed4f4b2
Edgar Yamill Parada Duarte <104601278+EdgarParada03@users.noreply.github.com>
2022-07-26 19:03:21 -0500

    Add files via upload

---
+++ Asteroids War Game/Asteroids/__main__.py
@@ -0,0 +1,6 @@
+from game import Asteroids
+
+if __name__ == "__main__":
+    _asteroids = Asteroids()
+    _asteroids.main_loop()
+    
---
+++ Asteroids War Game/Asteroids/game.py
@@ -0,0 +1,115 @@
+import pygame #Import pygame library
+
+from utils import get_random_position, load_sprite, print_text 
+from models import Asteroid, Spaceship
+
+class Asteroids: #Create the Asteroids class
+    MIN_ASTEROID_DISTANCE = 250
+    
+    def __init__(self): #Start pygame
+        self._init_pygame()
+        self.screen = pygame.display.set_mode((800, 600))
+        self.background = load_sprite("space", False)
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font(None, 64)
+        self.message = ""
+
+        self.asteroids = []
+        self.bullets = []
+        self.spaceship = Spaceship((400, 300), self.bullets.append)
+
+        for _ in range(6):
+            while True:
+                position = get_random_position(self.screen)
+                if (
+                    position.distance_to(self.spaceship.position)
+                    > self.MIN_ASTEROID_DISTANCE
+                ):
+                    break
+
+            self.asteroids.append(Asteroid(position, self.asteroids.append))
+
+    
+    def _get_game_objects(self):
+        return [*self.asteroids, self.spaceship]
+
+    def main_loop(self): #It's the game loop
+        while True:
+            self._handle_input() #contains the input handling.
+            self._process_game_logic() #Contine the logic of the game
+            self._draw() #Contains the drawings
+
+    def _init_pygame(self): #Initialize pygame
+        pygame.init() 
+        pygame.display.set_caption("Asteroids War") #Pygame program is titled
+
+    def _handle_input(self):
+        for event in pygame.event.get(): #Close the game with "ESC".
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+            ):
+                quit()
+            elif (
+                self.spaceship
+                and event.type == pygame.KEYDOWN
+                and event.key == pygame.K_SPACE
+            ):
+                self.spaceship.shoot()
+
+        is_key_pressed = pygame.key.get_pressed()
+        
+        #To control turns and acceleration
+        if self.spaceship:
+            if is_key_pressed[pygame.K_RIGHT]:
+                self.spaceship.rotate(clockwise=True)
+            elif is_key_pressed[pygame.K_LEFT]:
+                self.spaceship.rotate(clockwise=False)
+            if is_key_pressed[pygame.K_UP]:
+                self.spaceship.accelerate()
+
+    def _process_game_logic(self):
+        for game_object in self._get_game_objects():
+            game_object.move(self.screen)
+
+        if self.spaceship:
+            for asteroid in self.asteroids:
+                if asteroid.collides_with(self.spaceship):
+                    self.spaceship = None
+                    self.message = "You Dead!"
+                    break
+
+        for bullet in self.bullets[:]:
+            for asteroid in self.asteroids[:]:
+                if asteroid.collides_with(bullet):
+                    self.asteroids.remove(asteroid)
+                    self.bullets.remove(bullet)
+                    asteroid.split()
+                    break
+
+        for bullet in self.bullets[:]:
+            if not self.screen.get_rect().collidepoint(bullet.position):
+                self.bullets.remove(bullet)
+
+        if not self.asteroids and self.spaceship:
+            self.message = "Magistral Victory!"
+
+    def _draw(self): #Bringing life to the core workforce
+        self.screen.blit(self.background, (0, 0))
+
+        for game_object in self._get_game_objects():
+            game_object.draw(self.screen)
+
+        if self.message:
+            print_text(self.screen, self.message, self.font)
+
+        pygame.display.flip()
+        self.clock.tick(60)
+        
+    def _get_game_objects(self):
+        game_objects = [*self.asteroids, *self.bullets]
+
+        if self.spaceship:
+            game_objects.append(self.spaceship)
+
+        return game_objects
+
